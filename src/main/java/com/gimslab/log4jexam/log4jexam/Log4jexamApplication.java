adfffb7739a2853fb33f175b1178d43db19e6ee2
gimslab.com <gimslab.com@gmail.com>
2019-12-12 21:36:42 +0900

    log4jexam

---
+++ src/main/java/com/gimslab/log4jexam/log4jexam/Log4jexamApplication.java
@@ -0,0 +1,13 @@
+package com.gimslab.log4jexam.log4jexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4jexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Log4jexamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gimslab/log4jexam/log4jexam/Log4jexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimslab.log4jexam.log4jexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4jexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
