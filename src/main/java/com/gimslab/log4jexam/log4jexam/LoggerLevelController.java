e1d20beea00709cbe7710be475b1cb61d4cbd070
gmislab.com <gimslab.com@gmail.com>
2019-12-13 18:02:35 +0900

    getAllLogger

--- src/main/java/com/gimslab/log4jexam/log4jexam/LoggerLevelController.java
+++ src/main/java/com/gimslab/log4jexam/log4jexam/LoggerLevelController.java
@@ -1,6 +1,7 @@
 package com.gimslab.log4jexam.log4jexam;
 
 import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 import com.gimslab.log4jexam.log4jexam.pkga.ServiceA;
 import com.gimslab.log4jexam.log4jexam.pkgb.ServiceB;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 @RestController
 @Slf4j
 public class LoggerLevelController {
@@ -22,6 +25,19 @@ public class LoggerLevelController {
 	@GetMapping("/level")
 	public List<String> allLevel() {
 		return getEffectiveLevels();
+	}
+
+	@GetMapping("/loggers")
+	public List<String> getAllLoggers(){
+		val ctx = (LoggerContext)LoggerFactory.getILoggerFactory();
+		val loggers = ctx.getLoggerList();
+//		val root = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+//		val ctx = root.getLoggerContext();
+		return loggers.stream().map(this::loggerToString).collect(toList());
+	}
+
+	private String loggerToString(ch.qos.logback.classic.Logger logger) {
+		return logger.getName() + " " + logger.getLevel() + "   " + logger.getEffectiveLevel();
 	}
 
 	@PutMapping("/logger/{loggerName}/level/{level}")
