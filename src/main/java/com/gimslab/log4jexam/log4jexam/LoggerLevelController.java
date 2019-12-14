db7f3e2d0ecfab443a632f59c1d3414281bd601e
gimslab.com <gimslab.com@gmail.com>
2019-12-14 16:17:30 +0900

    deleteLogger()

--- src/main/java/com/gimslab/log4jexam/log4jexam/LoggerLevelController.java
+++ src/main/java/com/gimslab/log4jexam/log4jexam/LoggerLevelController.java
@@ -1,19 +1,13 @@
 package com.gimslab.log4jexam.log4jexam;
 
 import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
-import com.gimslab.log4jexam.log4jexam.pkga.ServiceA;
-import com.gimslab.log4jexam.log4jexam.pkgb.ServiceB;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -28,10 +22,21 @@ public class LoggerLevelController {
 	}
 
 	@PutMapping("/logger/{loggerName}/level/{level}")
-	public List<String> level(@PathVariable String loggerName, @PathVariable String level) {
-		val logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(loggerName);
-		logger.setLevel(Level.valueOf(level));
+	public List<String> updateLoggerLevel(@PathVariable String loggerName, @PathVariable String level) {
+		getLoggerByName(loggerName)
+				.setLevel(Level.valueOf(level));
 		return getAllLoggers(false);
+	}
+
+	@DeleteMapping("/logger/{loggerName}")
+	public List<String> deleteLogger(@PathVariable String loggerName){
+		getLoggerByName(loggerName)
+				.setLevel(null);
+		return getAllLoggers(false);
+	}
+
+	private Logger getLoggerByName(@PathVariable String loggerName) {
+		return (Logger) LoggerFactory.getLogger(loggerName);
 	}
 
 	private List<String> getAllLoggers(boolean all) {
@@ -44,10 +49,5 @@ public class LoggerLevelController {
 
 	private String loggerToString(ch.qos.logback.classic.Logger logger) {
 		return logger.getName() + " " + logger.getLevel() + "   " + logger.getEffectiveLevel();
-	}
-
-	private String level(String loggerName) {
-		val logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(loggerName);
-		return logger.getEffectiveLevel().toString();
 	}
 }
